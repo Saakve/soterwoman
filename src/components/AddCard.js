1db3e40ad87eb5704f416507c1222b8abe421150
Kevin Avelino <avesanke@gmail.com>
2023-05-03 18:15:14 -0600

    Added error handler

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -14,7 +14,12 @@ export function AddCard({ navigation }) {
 
     const assingIdStripe = async (idStripe) => {
         const { error } = await supabase.from('profile').update({ idstripe: idStripe }).eq('id', userData.id)
-        if (error) console.log(error)
+
+        if (error) {
+            handleError(error)
+            return
+        }
+
         setUserData((previusState) => ({ ...previusState, idStripe }))
     }
 
@@ -29,14 +34,20 @@ export function AddCard({ navigation }) {
         })
 
         if (error) {
-            Alert.alert(error)
+            handleError(error)
             return
         }
 
         if (userData.idStripe) {
-            const response = await createDriverCard({ idAccount: userData.idStripe, tokenCard: id })
+            const { error } = await createDriverCard({ idAccount: userData.idStripe, tokenCard: id })
+
+            if (error) {
+                handleError(error)
+                return
+            }
+
         } else {
-            const { idaccount } = await createFirstDriverCard({
+            const { idaccount, error } = await createFirstDriverCard({
                 tokenCard: id,
                 rfc: "000000000",
                 name: userData.name,
@@ -54,23 +65,41 @@ export function AddCard({ navigation }) {
                 }
             })
 
-            assingIdStripe(idaccount)
+            if (error) {
+                handleError(error)
+                return
+            }
+
+            await assingIdStripe(idaccount)
         }
         navigation.goBack()
     }
 
     const addPassengerCard = async ({ name, postalCode }) => {
         let clientSecret
+
         if (userData.idStripe) {
-            const { client_secret } = await createPassengerPaymentMethod({ idCustomer: userData.idStripe })
+            const { client_secret, error } = await createPassengerPaymentMethod({ idCustomer: userData.idStripe })
+
+            if (error) {
+                handleError(error)
+                return
+            }
+
             clientSecret = client_secret
         } else {
-            const { idcustomer, client_secret } = await createFirstPassengerPaymentMethod()
-            assingIdStripe(idcustomer)
+            const { idcustomer, client_secret, error } = await createFirstPassengerPaymentMethod()
+
+            if (error) {
+                handleError(error)
+                return
+            }
+
+            await assingIdStripe(idcustomer)
             clientSecret = client_secret
         }
 
-        const { setupIntent, error } = await confirmSetupIntent(clientSecret, {
+        const { error } = await confirmSetupIntent(clientSecret, {
             paymentMethodType: 'Card',
             paymentMethodData: {
                 billingDetails: {
@@ -82,13 +111,37 @@ export function AddCard({ navigation }) {
             }
         })
 
-        if (error) console.log(error)
+        if (error) handleError(error)
         else navigation.goBack()
     }
 
     const handleSavePaymentMethod = async ({ name, postalCode }) => {
         if (userData.idUserType === 1) await addDriverCard({ name, postalCode })
         else await addPassengerCard({ name, postalCode })
+    }
+
+    const handleError = (error) => {
+
+        if (typeof error === "string") {
+            Alert.alert("Error", "Hubo un problema, intente más tarde")
+            return
+        }
+
+        let message
+
+        if (error.stripeErrorCode === "card_declined") {
+            message = "Su tarjeta fue declinada. Contacte con el emisor de su tarjeta para más información."
+        } else if (error.stripeErrorCode === "expired_card") {
+            message = "La tarjeta ha caducado. Verifique la fecha de vencimiento o use otra tarjeta."
+        } else if (error.stripeErrorCode === "incorrect_cvc") {
+            message = "Código de seguridad incorrecto."
+        } else if (error.stripeErrorCode === "processing_error") {
+            message = "Ocurrió un error al procesar la tarjeta. Vuelva a intentarlo más tarde o con otra tarjeta."
+        } else if (error.stripeErrorCode === "incorrect_number") {
+            message = "Número de tarjeta incorrecto."
+        }
+
+        Alert.alert("Error", message)
     }
 
     return (
