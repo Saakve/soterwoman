cd9b2fd5e9d45da79399d4a5b06d1db2a546035f
kevind <kevin.dominguez2002@hotmail.com>
2023-05-24 22:11:24 -0600

    Continuepassenger trip basic flow

---
+++ src/components/ManageTripPassenger.js
@@ -0,0 +1,160 @@
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Call from "react-native-phone-call";
+import { Button, Icon } from "@rneui/base";
+
+import Avatar from "./Avatar";
+
+import { supabase } from "../services/supabase";
+import { TripPoints } from "./TripPoints";
+
+export function ManageTripPassenger({
+  trip,
+  origin,
+  destination,
+  onCancelledTrip = () => {},
+}) {
+  const [passenger, setPassenger] = useState(null);
+  const navigation = useNavigation();
+
+  const fetchPassenger = async () => {
+    const {
+      data: [passengerData],
+    } = await supabase.from("profile").select("*").eq("id", trip.idPassenger);
+    setPassenger(passengerData);
+  };
+
+  const triggerCall = () => {
+    const args = {
+      number: passenger.phone,
+      prompt: true,
+    };
+
+    Call(args).catch(console.error);
+  };
+
+  const displayMessage = () => {
+    navigation.navigate("Message", {
+      toUser: passenger,
+    });
+  };
+
+  useEffect(() => {
+    fetchPassenger();
+  }, []);
+
+  return (
+    <View style={styles.selector}>
+      <View style={styles.header}>
+        <Avatar
+          url={passenger?.idimage}
+          editable={false}
+          size={50}
+          style={styles.avatar}
+        />
+        <View style={styles.userdata}>
+          <Text style={styles.text}>{passenger?.name}</Text>
+          <View style={styles.ratinguser}>
+            <Icon name="star" type="font-awesome" color="#FFCC00" />
+            <Text style={styles.rating}>{passenger?.rating}</Text>
+          </View>
+        </View>
+        <View style={styles.tripdata}>
+          <Icon
+            name="comment-dots"
+            type="font-awesome-5"
+            solid
+            color="#4252FF"
+            size={35}
+            onPressOut={displayMessage}
+          />
+          <Icon
+            name="phone"
+            type="font-awesome"
+            color="#4CE5B1"
+            size={35}
+            onPressOut={triggerCall}
+          />
+        </View>
+      </View>
+      <TripPoints nameStartingpoint={origin} nameEndpoint={destination} />
+      <View style={styles.buttons}>
+        <Button
+          title={"Cancelar Viaje"}
+          buttonStyle={styles.button}
+          color="#B10710"
+          onPressOut={() => onCancelledTrip(trip)}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  selector: {
+    width: "100%",
+    height: "25%",
+    bottom: 0,
+    position: "absolute",
+    backgroundColor: "#FFF",
+  },
+  header: {
+    height: "35%",
+    backgroundColor: "#F7F7F7",
+    flexDirection: "row",
+    paddingHorizontal: "2%",
+    alignItems: "center",
+  },
+  avatar: {
+    marginTop: 0,
+  },
+  userdata: {
+    marginHorizontal: "5%",
+    justifyContent: "center",
+  },
+  ratinguser: {
+    flexDirection: "row",
+  },
+  rating: {
+    color: "#C8C7CC",
+    paddingLeft: "2%",
+  },
+  tripdata: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  servicetype: {
+    marginRight: "5%",
+    alignItems: "center",
+  },
+  children: {
+    marginLeft: "5%",
+    alignItems: "center",
+  },
+  trippoints: {
+    flexDirection: "row",
+    height: "30%",
+    marginBottom: "2%",
+  },
+  icons: {
+    marginHorizontal: "5%",
+    justifyContent: "space-evenly",
+  },
+  nametrippoints: {
+    justifyContent: "space-evenly",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  button: {
+    borderRadius: 10,
+    width: 163,
+    height: 50,
+  },
+  text: {
+    color: "#111",
+  },
+});
---
+++ src/pages/WaitSelector.js
@@ -0,0 +1,57 @@
+import { View, StyleSheet, Text, Image } from "react-native";
+import { Button } from "@rneui/base";
+
+export default function WaitSelector({
+  onPress = () => {}
+}) {
+
+  return (
+    <View style={styles.selector}>
+      <Image
+        source={require("../../assets/waitImage.png")}
+        style={styles.img}
+        resizeMode="contain"
+      />
+      <View style={styles.text_cancel}>
+        <Text style={{ fontSize: 18, textAlign: "center" }}>
+          Estamos buscando una conductora disponible, espera unos momentos...
+        </Text>
+
+        <Button
+          title="Cancelar"
+          buttonStyle={styles.button_cancel}
+          onPress={onPress}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  selector: {
+    width: "100%",
+    height: "35%",
+    position: "absolute",
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    bottom: Platform.select({ ios: "0%", android: "0%" }),
+  },
+  img: {
+    height: 185,
+    width: 302,
+    alignSelf: "center",
+  },
+  text_cancel: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "space-between",
+    width: "100%",
+  },
+  button_cancel: {
+    borderRadius: 10,
+    backgroundColor: "#8946A6",
+    width: "90%",
+  },
+});
